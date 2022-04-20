d2aea9585e39da809af77ed7cb9bd4a02cac44f2
Rai Apostol <ryan.apostol@transcom.com>
2022-04-20 18:44:48 +0800

    Implementation of repository pattern, auto db migration and seeding, additional endpoints for producst/types/brands

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,30 +1,44 @@
 using Core.Entities;
-using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllersauto
+namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        public StoreContext _context { get; }
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _productRepo;
+        public ProductsController(IProductRepository productRepo)
         {
-            _context = context;
+            _productRepo = productRepo;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts() {
-            var products = await _context.Products.ToListAsync();
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _productRepo.GetProductsAsync();
 
             return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id) {
-            return await _context.Products.FindAsync(id);
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _productRepo.GetProductByIdAsync(id);
         }
+        
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productRepo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _productRepo.GetProductTypesAsync());
+        }
+        
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,44 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<StoreContext>(x => 
-    x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+{
+    var services = builder.Services;
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    services.AddScoped<IProductRepository, ProductRepository>();
+
+    services.AddControllers();
+    services.AddDbContext<StoreContext>(x =>
+        x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+
+}
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error occured during migration");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
