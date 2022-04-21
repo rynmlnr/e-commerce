b6e6030cbc0be064f8b375b3acf171e4bd41826b
Rai Apostol <ryan.apostol@transcom.com>
2022-04-21 21:30:48 +0800

    Added Navbar, models and Get Product

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination, Product } from './models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'E-Commerce App';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<Pagination>('https://localhost:7203/api/products?pageSize=50').subscribe(
+      (response: Pagination) => {
+        this.products = response.data;
+      }, error => {
+        console.log(error);
+      }
+    )
+  }
 }
